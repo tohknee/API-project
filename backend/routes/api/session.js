85c9a816241c26356aa1004831c58081c29f6045
Tony He <iamt.he@yahoo.com>
2023-04-14 08:06:01 -0700

    add user input validation on user login backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const { Op } = require("sequelize");
 const bcrypt = require("bcryptjs");
-
+//import setTokenCookie and restore user function.
 const { setTokenCookie, restoreUser } = require("../../utils/auth");
 const { User } = require("../../db/models");
 const router = express.Router();
+//import check function and handleValidationError functions. they will validate the body of a request
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
 
 // Log in
 router.post("/", async (req, res, next) => {
@@ -61,5 +64,51 @@ router.get("/", (req, res) => {
   } else return res.json({ user: null });
 });
 
+//validateLogin middleware checks the credential key with username or email and the key password with the password
+//this middleware is made of checks. checks if req.body.credential and req.body.password are empty
+const validateLogin = [
+  check("credential")
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage("Please provide a valid email or username."),
+  check("password")
+    .exists({ checkFalsy: true })
+    .withMessage("Please provide a password."),
+  handleValidationErrors,
+];
+
+// Log in post route
+router.post("/", validateLogin, async (req, res, next) => {
+  const { credential, password } = req.body;
+
+  const user = await User.unscoped().findOne({
+    where: {
+      [Op.or]: {
+        username: credential,
+        email: credential,
+      },
+    },
+  });
+
+  if (!user || !bcrypt.compareSync(password, user.hashedPassword.toString())) {
+    const err = new Error("Login failed");
+    err.status = 401;
+    err.title = "Login failed";
+    err.errors = { credential: "The provided credentials were invalid." };
+    return next(err);
+  }
+
+  const safeUser = {
+    id: user.id,
+    email: user.email,
+    username: user.username,
+  };
+
+  await setTokenCookie(res, safeUser);
+
+  return res.json({
+    user: safeUser,
+  });
+});
 //export router
 module.exports = router;
