f524618ee3c09120dd1104f95f98a8e840ee4688
Tony He <iamt.he@yahoo.com>
2023-04-13 23:35:56 -0700

    added user login backend enpoint

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,4 +1,8 @@
 const router = require("express").Router();
+//connect session and users routers
+const sessionRouter = require("./session.js");
+const usersRouter = require("./users.js");
+const { restoreUser } = require("../../utils/auth.js");
 
 // //test api router. to test past fetch request into console and replace value of XSRF TOKEN
 // router.post("/test", function (req, res) {
@@ -29,5 +33,19 @@ const router = require("express").Router();
 // router.get("/require-auth", requireAuth, (req, res) => {
 //   return res.json(req.user);
 // });
-router.use(restoreUser);
+
+router.use(restoreUser); //restoreUsers middleware connection
+
+// Connect restoreUser middleware to the API router after restoreUser middleware connection above
+// If current user session is valid, set req.user to the user in the database
+// If current user session is not valid, set req.user to null
+
+router.use("/session", sessionRouter);
+
+router.use("/users", usersRouter);
+
+router.post("/test", (req, res) => {
+  res.json({ requestBody: req.body });
+});
+
 module.exports = router;
