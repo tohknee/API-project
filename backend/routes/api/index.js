65c1430120f9072a310cdafe08f36b153834c43d
Tony He <iamt.he@yahoo.com>
2023-04-13 17:24:47 -0700

    add uer authentication expres middleware

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,8 +1,33 @@
 const router = require("express").Router();
 
-//test api router. to test past fetch request into console and replace value of XSRF TOKEN
-router.post("/test", function (req, res) {
-  res.json({ requestBody: req.body });
-});
+// //test api router. to test past fetch request into console and replace value of XSRF TOKEN
+// router.post("/test", function (req, res) {
+//   res.json({ requestBody: req.body });
+// });
 
+// // GET /api/set-token-cookie test router
+// const { setTokenCookie } = require("../../utils/auth.js");
+// const { User } = require("../../db/models");
+// router.get("/set-token-cookie", async (_req, res) => {
+//   const user = await User.findOne({
+//     where: {
+//       username: "Demo-lition",
+//     },
+//   });
+//   setTokenCookie(res, user);
+//   return res.json({ user: user });
+// });
+
+// // GET /api/restore-user test router
+// const { restoreUser } = require("../../utils/auth.js");
+
+// router.get("/restore-user", (req, res) => {
+//   return res.json(req.user);
+// });
+
+// const { requireAuth } = require("../../utils/auth.js");
+// router.get("/require-auth", requireAuth, (req, res) => {
+//   return res.json(req.user);
+// });
+router.use(restoreUser);
 module.exports = router;
