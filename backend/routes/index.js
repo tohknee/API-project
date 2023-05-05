94346d511bffa3ef43679edb2de2ce39b55b4b94
Tony <iamt.he@yahoo.com>
2023-05-05 10:01:18 -0700

    frontend commit

---
+++ backend/routes/index.js
@@ -0,0 +1,23 @@
+// backend/routes/index.js
+const express = require("express");
+const router = express.Router();
+//connect backend/routes/api/index.js
+const apiRouter = require("./api");
+router.use("/api", apiRouter);
+
+//this is a test route. should display Hello World!
+router.get("/hello/world", function (req, res) {
+  res.cookie("XSRF-TOKEN", req.csrfToken()); //setting cookie on the response with name of XSRF-token and value of req.csrfToken methods return
+  res.send("Hello World!"); //send Hello World! as res body
+});
+
+// Add a XSRF-TOKEN cookie
+router.get("/api/csrf/restore", (req, res) => {
+  const csrfToken = req.csrfToken();
+  res.cookie("XSRF-TOKEN", csrfToken);
+  res.status(200).json({
+    "XSRF-Token": csrfToken,
+  });
+});
+
+module.exports = router;
